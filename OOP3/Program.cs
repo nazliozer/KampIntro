351ab26117ed086585c8ed71805d07fc7a11b2dd
Nazlı Hilal Özer <ozernazli62@gmail.com>
2021-01-24 00:17:46 +0300

    23 ocak ders sonu

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -14,10 +14,10 @@ namespace OOP3
             ILoggerService databaseLoggerService = new DatabaseLoggerService();
             ILoggerService fileLoggerservice = new FileLoggerService();
 
-
+            List<ILoggerService> loggers = new List<ILoggerService> {  new FileLoggerService(), new SmsLoggerService()};
 
             BasvuruManager basvuruManager = new BasvuruManager();
-            basvuruManager.BasvuruYap(new EsnafKredisiManager() , new SmsLoggerService());
+            basvuruManager.BasvuruYap(new EsnafKredisiManager() , loggers);
 
             List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager, taşıtKrediManager};
             
